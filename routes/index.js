74afb5ef66bf12f86faa4622391b90654a83eb9a
Olumide <olumideokedusi@gmail.com>
2018-12-02 07:17:13 +0100

    Implement the post route for searching gifs + formating to return a single gif

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,24 @@ router.get("/", function(req, res, next) {
   });
 });
 
+router.get("/search", (req, res, next) => {
+  res.render("search");
+});
+
+router.post("/search", (req, res, next) => {
+  const query = req.body["giphy-query"];
+  const url = `http://api.giphy.com/v1/gifs/search?api_key=aAcKJrwOggkKWc6KmFCSFlmQ0amFjUkf&q=${query}`;
+  request.get(url, (err, response, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      body = JSON.parse(body);
+      const randomResult =
+        body.data[Math.floor(Math.random() * body.data.length)];
+      const searchResultUrl = randomResult.images.fixed_height.url;
+      res.render("search", { searchResultUrl });
+    }
+  });
+});
+
 module.exports = router;
